ab75faef8b03e5ea2770289fc227ea91ebaa3bb5
Basil Crow <me@basilcrow.com>
2025-08-07 08:53:44 -0700

    Bump nix from 0.26 to 0.27 (#7)

--- src/bin/testing/epoll.rs
+++ src/bin/testing/epoll.rs
@@ -1,8 +1,9 @@
-use nix::sys::epoll;
+use nix::sys::epoll::{Epoll, EpollCreateFlags, EpollEvent, EpollFlags};
 use nix::unistd::pipe2;
 use nix::fcntl::OFlag;
 
 use std::fs::File;
+use std::os::fd::{FromRawFd, OwnedFd};
 
 extern crate nix;
 
@@ -10,9 +11,10 @@ fn main() {
 
     let (readfd, _writefd) = pipe2(OFlag::O_CLOEXEC | OFlag::O_NONBLOCK).unwrap();
 
-    let epollfd = epoll::epoll_create().unwrap();
-    let mut event = epoll::EpollEvent::new(epoll::EpollFlags::EPOLLIN, 0);
-    epoll::epoll_ctl(epollfd, epoll::EpollOp::EpollCtlAdd, readfd, Some(&mut event)).unwrap();
+    let epoll = Epoll::new(EpollCreateFlags::empty()).unwrap();
+    let event = EpollEvent::new(EpollFlags::EPOLLIN, 0);
+    let owned_readfd: OwnedFd = unsafe { OwnedFd::from_raw_fd(readfd) };
+    epoll.add(&owned_readfd, event).unwrap();
 
     // Signal parent process (the test process) that this process is ready to be observed by the
     // ptool being tested.
