0bf82186de071e11cefc3f3d7a1b098501ae2993
Basil Crow <me@basilcrow.com>
2026-02-23 15:44:44 -0800

    Update copyright format

--- tests/pargs_penv_test.rs
+++ tests/pargs_penv_test.rs
@@ -1,5 +1,5 @@
 //
-//   Copyright 2026 Basil Crow
+//   Copyright (c) 2026 Basil Crow
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
